e81c8f5f5492bf6928382075dd4416b2eb00d5a8
Sopheackanha <hort.sopheackanha24@kit.edu.kh>
2025-12-25 12:21:22 +0700

    update the model and load dataset

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,27 +1,32 @@
 from django.db import models
+from django.contrib.postgres.indexes import GinIndex
 
 # Create your models here.
 class ProhibitedIngredient(models.Model):
     """EU Annex II Prohibited substances"""
-    name = models.CharField(max_length=500, db_index=True)
-    name_normalized = models.CharField(max_length=500, db_index=True, help_text="Lowercase, trimmed version")
-    cas_number = models.CharField(max_length=50, blank=True, null=True, db_index=True)
-    annex_ref = models.CharField(max_length=200)
+    name = models.TextField(db_index=True)
+    name_normalized = models.TextField(db_index=True, help_text="Lowercase, trimmed version")
+    cas_number = models.CharField(blank=True, null=True, db_index=True)
+    ec_number = models.CharField(blank=True, null=True)
+    regulation = models.CharField(blank=True, null=True)
     is_cmr = models.BooleanField(default=False, help_text="Carcinogenic, Mutagenic, or Toxic for Reproduction")
-    notes = models.TextField(blank=True)
+    cmr_note = models.TextField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # define metadata of the model
     class Meta:
         db_table = 'prohibited_ingredients'
         indexes = [
+            models.Index(fields=['name']),
             models.Index(fields=['name_normalized']),
             models.Index(fields=['cas_number']),
+            GinIndex(fields=['name'], name='name_gin_trgm', opclasses=['gin_trgm_ops']), #for partial or fuzzy match when search
         ]
 
     def __str__(self):
-        return f"{self.name} ({self.annex_ref})"
+        return f"{self.name}"
     
 # not yet make migration
 # can make migration with command:
--- cosing_checker/settings.py
+++ cosing_checker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'analyzer',
     'django_extensions',
 ]
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'cosing_checker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default=5432, cast=int)
     }
 }
 
